930b2c014d509df18fc404e87c57ebc740d72e5d
Marcus Lai <marcus41881653@gmail.com>
2023-06-29 00:29:40 +0800

    improved visualization

--- euler.py
+++ euler.py
@@ -21,9 +21,11 @@ def euler(func, start_vec, h, steps):
     output_eulers = []
     for col in range(num_columns):
         if col % 3 == 0:
+            subdf_index = (col // 3) + 1
             euler_subdf = euler_df[[str(col), str(col+1), str(col+2)]].copy()
             euler_subdf.rename(columns={str(col): "x", str(col+1): "y", str(col+2): "z"}, inplace=True)
             output_eulers.append(euler_subdf)
+            euler_subdf.to_csv(f"data_{subdf_index}.csv")
 
     return output_eulers
 
@@ -33,20 +35,33 @@ def plot_euler(euler_df):
 
 #-----------------------------------------------------------------
 "ANIMATION"
+
+COLORS = ["blue", "orange", "green", "red"]
+
 def animate(euler_df):
     steps = len(euler_df[0].index)
 
     curves = []
+    curve_supports = []
     for curve_index in range(len(euler_df)):
-        new_curve, = AX.plot([],[],[],".", alpha=0.4, markersize=20, label=curve_index+1)
+        color = COLORS[curve_index % 4]
+        new_curve, = AX.plot([],[],[],".", alpha=0.4, markersize=20, label=curve_index+1, c=color)
+        x_support, = AX.plot([],[],[],"o:", alpha=0.4, markersize=3, linewidth=1, c=color)
+        y_support, = AX.plot([],[],[],"o:", alpha=0.4, markersize=3, linewidth=1, c=color)
+        z_support, = AX.plot([],[],[],"o:", alpha=0.4, markersize=3, linewidth=1, c=color)
+
         curves.append(new_curve)
+        curve_supports.append([x_support, y_support, z_support])
 
     # only for 4 cells
-    axis1, = AX.plot([],[],[],":", alpha=0.1, markersize=0, c="black")
-    axis2, = AX.plot([],[],[],":", alpha=0.1, markersize=0, c="black")
+    axis1, = AX.plot([],[],[],":", alpha=0.4, markersize=0, c="black")
+    axis2, = AX.plot([],[],[],":", alpha=0.4, markersize=0, c="black")
+
+
+
 
     anim = FuncAnimation(FIG, update_replace, steps, 
-                        fargs=(curves, axis1, axis2, euler_df)
+                        fargs=(curves, curve_supports, axis1, axis2, euler_df)
                         ,interval=500
                         ,repeat=True)
 
@@ -64,7 +79,6 @@ def animate(euler_df):
     anim.save("YZ.gif")
 
 
-
 # def update(frame, curve, x, y, z, data):
 #     x.append(data["x"][frame])
 #     y.append(data["y"][frame])
@@ -73,15 +87,41 @@ def animate(euler_df):
 #     curve.set_3d_properties(z)
 #     return curve, 
 
-def update_replace(frame, curves, axis1, axis2, data):
+def update_replace(frame, curves, curve_supports, axis1, axis2, data):
 
     for curve_index in range(len(curves)):
-        x = [data[curve_index]["x"][frame]]
-        y = [data[curve_index]["y"][frame]]
+        x = data[curve_index]["x"][frame]
+        y = data[curve_index]["y"][frame]
         z = data[curve_index]["z"][frame]
 
-        curves[curve_index].set_data([x, y])
+        curves[curve_index].set_data([[x], [y]])
         curves[curve_index].set_3d_properties([z])
+
+        # x supports
+        # curve_supports[curve_index][0].set_data([[x, x], [0, y]])
+        # curve_supports[curve_index][0].set_3d_properties([0, z])
+
+        # # y supports
+        # curve_supports[curve_index][1].set_data([[0, x], [y, y]])
+        # curve_supports[curve_index][1].set_3d_properties([0, z])
+
+        # # z supports
+        # curve_supports[curve_index][2].set_data([[0, x], [0, y]])
+        # curve_supports[curve_index][2].set_3d_properties([z, z])
+
+        # xy supports
+        curve_supports[curve_index][0].set_data([[x, x], [y, y]])
+        curve_supports[curve_index][0].set_3d_properties([z, -SIZE])
+
+        # xz supports
+        curve_supports[curve_index][1].set_data([[x, -SIZE], [y, y]])
+        curve_supports[curve_index][1].set_3d_properties([z, z])
+
+        # yz supports
+        curve_supports[curve_index][2].set_data([[x, x], [y, SIZE]])
+        curve_supports[curve_index][2].set_3d_properties([z, z])
+
+
 
     # only for 4 cells
     axis1.set_data([[data[0]["x"][frame], data[2]["x"][frame]],[data[0]["y"][frame], data[2]["y"][frame]]])
@@ -169,10 +209,17 @@ def physics_system(vector):
 # setup
 FIG = plt.figure()
 AX = FIG.add_subplot(projection="3d")
+SIZE = 2
 # Setting the axes properties
-AX.set(xlim3d=(-2, 2), xlabel='X')
-AX.set(ylim3d=(-2, 2), ylabel='Y')
-AX.set(zlim3d=(-2, 2), zlabel='Z')
+AX.set(xlim3d=(-SIZE, SIZE), xlabel='X')
+AX.set(ylim3d=(-SIZE, SIZE), ylabel='Y')
+AX.set(zlim3d=(-SIZE, SIZE), zlabel='Z')
+
+# AX.grid(False)
+
+# xaxis, = AX.plot([SIZE, -SIZE],[0, 0],[0, 0],"-", alpha=0.2, c="black")
+# yaxis, = AX.plot([0, 0],[SIZE, -SIZE],[0, 0],"-", alpha=0.2, c="black")
+# zaxis, = AX.plot([0, 0],[0, 0],[SIZE, -SIZE],"-", alpha=0.2, c="black")
 
 # print(physics_system([-1,1,1,1,1,1,-1,-1,1,-1,1,1]))
 

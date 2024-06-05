e90d59b51795d68119535befd90b933fdf0a4944
marclai <marclai@student.ubc.ca>
2024-06-04 20:10:14 -0700

    resolving pushing problem

--- src/modelling/Fit/fit_config.py
+++ src/modelling/Fit/fit_config.py
@@ -5,7 +5,7 @@ INIT = (0.5, 0.5, 0, 0.5, -0.5, 0, -0.5, -0.5, 0, -0.5, 0.5, 0)
 
 #====================== 2 parameter models ======================
 # MODEL AB
-# GET_VELOCITY = ModelAB.get_velocity
+GET_VELOCITY = ModelAB.get_velocity
 
 # MODEL EXTENDING SPRING
 # GET_VELOCITY = ModelES.get_velocity
@@ -14,7 +14,7 @@ INIT = (0.5, 0.5, 0, 0.5, -0.5, 0, -0.5, -0.5, 0, -0.5, 0.5, 0)
 # GET_VELOCITY = ModelABp2.get_velocity
 
 # MODEL EXTENDING SPRING P2
-GET_VELOCITY = ModelESp2.get_velocity
+# GET_VELOCITY = ModelESp2.get_velocity
 
 # MODEL EXTENDING SPRING, Cell Wall Velocity
 # GET_VELOCITY = ModelESFriction.get_velocity
